13277f98bc65acb4ebc4f8817d39a394a378ae0c
liwenyu <liwenyu@tju.edu.cn>
2025-08-07 17:58:07 +0800

    第一次初始化

---
+++ springboot-test/src/main/java/com/demo/BasicApplication.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 引导类
+ * @author lwy
+ * @date 2025/08/07 17:43
+ **/
+@SpringBootApplication
+public class BasicApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BasicApplication.class, args);
+    }
+}
