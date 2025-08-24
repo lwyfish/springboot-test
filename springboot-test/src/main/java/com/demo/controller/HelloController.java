884a5db3793fff2ba639af1034fcff76c79a452e
lwy <liwenyu@tju.edu.cn>
2025-08-24 11:53:55 +0800

    配置、打包问题解决

--- springboot-test/src/main/java/com/demo/controller/HelloController.java
+++ springboot-test/src/main/java/com/demo/controller/HelloController.java
@@ -1,13 +1,22 @@
 package com.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${name}")
+    private String name;
+
     @RequestMapping("/hello")
     public String hello() {
         return "Hello SpringBoot!";
+    }
+
+    @RequestMapping("/hello2")
+    public String hello2() {
+        return name;
     }
 }
